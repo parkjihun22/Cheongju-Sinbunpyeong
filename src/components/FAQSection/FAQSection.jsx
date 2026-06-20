410dbaec49fc74cdbf734d0a3802962df541dea0
parkjihun22 <eyeful31@naver.com>
2026-06-20 15:30:23 +0900

    feat : 수정

--- src/components/FAQSection/FAQSection.jsx
+++ src/components/FAQSection/FAQSection.jsx
@@ -5,7 +5,7 @@ import faqData from "../../../src/faqData";
 
 const FAQSection = ({
   title = "Q & A",
-  subTitle = "고덕 수자인풍경채 자주 묻는 질문",
+  subTitle = "대전 문화공원 수자인 자주 묻는 질문",
   data = faqData,
 }) => {
   const [openIndex, setOpenIndex] = useState(0);
